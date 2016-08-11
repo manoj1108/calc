6c839c6f29900d9d09b1ae8e1f0352499c274979
dev2 <manoj.rajavolu@gmail.com>
2016-08-11 13:43:56 +0530

    lunch

--- kumar2/src/main/java/com/calc/Calc.java
+++ kumar2/src/main/java/com/calc/Calc.java
@@ -8,20 +8,16 @@
 package com.calc;
 
 public class Calc {
-	public double add(double a, double b)
-	{
-		
-	
-	double res=a+b;
-	return res;
-}
-	
-	public double sub(double a, double b)
-	{
-		
-	
-	 double res = a-b;
-	return res;
-}
-	
+	public double add(double a, double b) {
+
+		double res = a + b;
+		return res;
+	}
+
+	public double sub(double a, double b) {
+
+		double res = a - b;
+		return res;
+	}
+
 }
